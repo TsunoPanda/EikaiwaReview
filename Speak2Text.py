749443ed54e0b6c3755b633f1a1fe51ad71b668d
KABOTAN-MK2\tsunk <tsunkawa4@gmail.com>
2025-08-08 10:01:09 +0900

    Improve comments

--- Speak2Text.py
+++ Speak2Text.py
@@ -1,43 +1,47 @@
 import whisper
 import time
+import sys
 
 def transcribe_audio(file_path, model_size="base"):
-    """
-    Transcribe audio file to text using Whisper ASR.
-    
-    Args:
-        file_path (str): Path to the audio file.
-    
-    Returns:
-        str: Transcribed text.
-    """
-    model = whisper.load_model(model_size)
-    result = model.transcribe(file_path, language = "en")
-    return result['text']
+  """
+  Transcribe audio file to text using Whisper ASR.
+  
+  Args:
+      file_path (str): Path to the audio file.
+  
+  Returns:
+      str: Transcribed text.
+  """
+  model = whisper.load_model(model_size)
+  result = model.transcribe(file_path, language = "en")
+  return result['text']
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 3:
-        print("Usage: python Speak2Text.py <audio_file_path> <model_size>")
-        print("Example: python Speak2Text.py audio.wav base")
-        sys.exit(1)
-    
-    audio_file_path = sys.argv[1]
-    model_size = sys.argv[2]
-    if model_size not in ["tiny", "base", "small", "medium", "large"]:
-        print("Using 'base' model.")
-        model_size = "base"
+  # Check command line arguments
+  if len(sys.argv) != 3:
+    print("Usage: python Speak2Text.py <audio_file_path> <model_size>")
+    print("Example: python Speak2Text.py audio.wav base")
+    sys.exit(1)
+  
+  # Get audio file path and model size from command line arguments
+  audio_file_path = sys.argv[1]
+  model_size = sys.argv[2]
 
-    try:
-        start_time = time.time()  # Start timing
-        transcription = transcribe_audio(audio_file_path, model_size)
-        end_time = time.time()  # End timing
+  # Validate model size
+  if model_size not in ["tiny", "base", "small", "medium", "large"]:
+    print("Using 'base' model.")
+    model_size = "base"
 
-        with open("transcription.txt", "w", encoding="utf-8") as f:
-          f.write(transcription)
+  try:
+    start_time = time.time()  # Start timing
+    transcription = transcribe_audio(audio_file_path, model_size)
+    end_time = time.time()  # End timing
 
-        print(f"Elapsed time: {end_time - start_time:.2f} seconds")
-    except Exception as e:
-        print(f"Error transcribing audio: {e}")
-        sys.exit(1)
+    with open("transcription.txt", "w", encoding="utf-8") as f:
+      f.write(transcription)
+
+    print(f"Elapsed time: {end_time - start_time:.2f} seconds")
+  except Exception as e:
+    print(f"Error transcribing audio: {e}")
+    sys.exit(1)
 
